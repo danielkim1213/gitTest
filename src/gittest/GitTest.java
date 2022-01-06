9ecd99f6b8a20fe5b46c4e6db53745cc7d123b99
Daniel Kim <kyw4091@gmail.com>
2022-01-05 20:08:06 -0500

    last commit.

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -15,7 +15,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         System.out.println("GitHub");
-        System.out.println("HAHA");
+        System.out.println("alskdfj");
     }
     
 }
